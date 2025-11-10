65b53784e00bf2bc49c1ca00616d8a45cb5e426e
omer-io <omer.jamil@rakurai.io>
2025-11-10 11:30:42 +0000

    Rakurai Patch

--- local-cluster/src/validator_configs.rs
+++ local-cluster/src/validator_configs.rs
@@ -87,6 +87,12 @@ pub fn safe_clone_config(config: &ValidatorConfig) -> ValidatorConfig {
         shred_retransmit_receiver_addresses: config.shred_retransmit_receiver_addresses.clone(),
         tip_manager_config: config.tip_manager_config.clone(),
         bam_url: config.bam_url.clone(),
+        reward_distribution_config: config.reward_distribution_config.clone(),
+        rakurai_config: config.rakurai_config.clone(),
+        target_slot_adjustment_ms: config.target_slot_adjustment_ms,
+        tx_io_check: config.tx_io_check.clone(),
+        oms_connector: config.oms_connector,
+        client_mode: config.client_mode.clone(),
     }
 }
 
