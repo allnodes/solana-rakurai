6baa226f0ac46f9509d42848a3dbe662579b7637
omer-io <omer.jamil@rakurai.io>
2025-11-10 11:30:42 +0000

    Rakurai Patch

--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -1,6 +1,14 @@
 //! The `tpu` module implements the Transaction Processing Unit, a
 //! multi-stage transaction processing pipeline in software.
 
+// allow multiple connections for NAT and any open/close overlap
+use crate::banking_stage::{house_keeper::HouseKeeper, DecisionState};
+#[deprecated(
+    since = "2.2.0",
+    note = "Use solana_streamer::quic::DEFAULT_MAX_QUIC_CONNECTIONS_PER_PEER instead"
+)]
+pub use solana_streamer::quic::DEFAULT_MAX_QUIC_CONNECTIONS_PER_PEER as MAX_QUIC_CONNECTIONS_PER_PEER;
+
 pub use crate::forwarding_stage::ForwardingClientOption;
 use {
     crate::{
@@ -9,7 +17,9 @@ use {
         bam_manager::BamManager,
         banking_stage::consumer::TipProcessingDependencies,
         banking_stage::{
+            reward_distributor::RewardDistributionConfig,
             transaction_scheduler::scheduler_controller::SchedulerConfig, BankingStage,
+            RakuraiConfig,
         },
         banking_trace::{Channels, TracerThread},
         // bundle_stage::bundle_account_locker::BundleAccountLocker,
@@ -31,7 +41,7 @@ use {
         staked_nodes_updater_service::StakedNodesUpdaterService,
         // tip_manager::{TipManager, TipManagerConfig},
         tpu_entry_notifier::TpuEntryNotifier,
-        validator::{BlockProductionMethod, GeneratorConfig},
+        validator::{BlockProductionMethod, ClientMode, GeneratorConfig},
         vortexor_receiver_adapter::VortexorReceiverAdapter,
     },
     arc_swap::ArcSwap,
@@ -135,6 +145,7 @@ pub struct Tpu {
     sig_verifier: SigVerifier,
     vote_sigverify_stage: SigVerifyStage,
     banking_stage: Arc<RwLock<Option<BankingStage>>>,
+    house_keeper_thread: HouseKeeper,
     forwarding_stage: JoinHandle<()>,
     cluster_info_vote_listener: ClusterInfoVoteListener,
     broadcast_stage: BroadcastStage,
@@ -202,6 +213,11 @@ impl Tpu {
         tip_manager_config: TipManagerConfig,
         shred_receiver_addresses: Arc<ArcSwap<ShredReceiverAddresses>>,
         bam_url: Arc<ArcSwap<Option<String>>>,
+        reward_distribution_config: RewardDistributionConfig,
+        rakurai_config: Arc<RwLock<RakuraiConfig>>,
+        tx_io_check: Option<String>,
+        oms_connector: bool,
+        client_mode: Arc<Mutex<ClientMode>>,
     ) -> Self {
         let TpuSockets {
             transactions: transactions_sockets,
@@ -327,6 +343,30 @@ impl Tpu {
             (None, None)
         };
 
+        const TX_IO_CHANNEL_SZIE: usize = 100_000;
+        let enable_tx_io_check = tx_io_check.is_some();
+        let (input_tx_signature_sender, input_tx_signature_receiver) = if enable_tx_io_check {
+            let (input_tx_signature_sender, input_tx_signature_receiver) =
+                bounded(TX_IO_CHANNEL_SZIE);
+            (
+                Some((input_tx_signature_sender, exit.clone())),
+                Some(input_tx_signature_receiver),
+            )
+        } else {
+            (None, None)
+        };
+        let (output_tx_signature_sender, output_tx_signature_receiver) =
+            if enable_tx_io_check || oms_connector {
+                let (output_tx_signature_sender, output_tx_signature_receiver) =
+                    bounded(TX_IO_CHANNEL_SZIE);
+                (
+                    Some(output_tx_signature_sender),
+                    Some(output_tx_signature_receiver),
+                )
+            } else {
+                (None, None)
+            };
+
         let (forward_stage_sender, forward_stage_receiver) = bounded(1024);
         let sig_verifier = if let Some(vortexor_receivers) = vortexor_receivers {
             info!("starting vortexor adapter");
@@ -344,6 +384,7 @@ impl Tpu {
             let verifier = TransactionSigVerifier::new(
                 banking_stage_sender.clone(),
                 enable_block_production_forwarding.then(|| forward_stage_sender.clone()),
+                input_tx_signature_sender.clone(),
             );
             SigVerifier::Local(SigVerifyStage::new(
                 sigverify_stage_receiver,
@@ -385,12 +426,14 @@ impl Tpu {
             &block_builder_fee_info,
             shredstream_receiver_address.clone(),
             bam_enabled.clone(),
+            input_tx_signature_sender.clone(),
         );
         let (verified_bundle_sender, verified_bundle_receiver) = bounded(1024);
         let bundle_sigverify_stage = BundleSigverifyStage::new(
             unverified_bundle_receiver,
             verified_bundle_sender,
             exit.clone(),
+            banking_stage_sender.clone(),
         );
 
         let bam_tpu_info = Arc::new(ArcSwap::new(Arc::new(None)));
@@ -447,6 +490,10 @@ impl Tpu {
             bam_tpu_info,
         };
 
+        let shared_decision = (
+            Arc::new(RwLock::new(DecisionState::Hold)),
+            Arc::new(AtomicBool::new(false)),
+        );
         let mut blacklisted_accounts = HashSet::new();
         blacklisted_accounts.insert(tip_manager.tip_payment_program_id());
 
@@ -474,6 +521,25 @@ impl Tpu {
                 bundle_account_locker: bundle_account_locker.clone(),
             }),
             Some(bam_dependencies.clone()),
+            cluster_info,
+            blockstore.clone(),
+            reward_distribution_config,
+            rakurai_config,
+            input_tx_signature_sender.clone(),
+            output_tx_signature_sender,
+            shared_decision.clone(),
+            exit.clone(),
+            client_mode.clone(),
+        );
+
+        // House keeper
+        let house_keeper_thread = HouseKeeper::new(
+            input_tx_signature_receiver,
+            output_tx_signature_receiver,
+            tx_io_check,
+            oms_connector,
+            shared_decision,
+            exit.clone(),
         );
 
         let SpawnForwardingStageResult {
@@ -511,6 +577,8 @@ impl Tpu {
             bam_dependencies,
             poh_recorder.clone(),
             key_notifiers.clone(),
+            banking_stage_sender.clone(),
+            client_mode.clone(),
         );
 
         let (entry_receiver, tpu_entry_notifier) =
@@ -558,6 +626,7 @@ impl Tpu {
             sig_verifier,
             vote_sigverify_stage,
             banking_stage: Arc::new(RwLock::new(Some(banking_stage))),
+            house_keeper_thread,
             forwarding_stage,
             cluster_info_vote_listener,
             broadcast_stage,
@@ -592,6 +661,7 @@ impl Tpu {
                 .take()
                 .expect("banking_stage must be Some")
                 .join(),
+            self.house_keeper_thread.join(),
             self.forwarding_stage.join(),
             self.staked_nodes_updater_service.join(),
             self.tpu_quic_t.map_or(Ok(()), |t| t.join()),
