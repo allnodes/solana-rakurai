4d69d68a68ca9a6d06b2d648e9eeaf8b62e5cf3e
Sephiroth <sephiroth@abyss.finance>
2026-07-08 19:51:17 +0000

    Allnodes Patch

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -128,8 +128,10 @@ mod tpu_to_pack;
 const MAX_NUM_WORKERS: NonZeroUsize = NonZeroUsize::new(64).unwrap();
 const DEFAULT_NUM_WORKERS: NonZeroUsize = NonZeroUsize::new(4).unwrap();
 
+allnodes_client::constants! {
 pub const TOTAL_BUFFERED_PACKETS: usize = 100_000;
 pub const SLOT_BOUNDARY_CHECK_PERIOD: Duration = Duration::from_millis(10);
+}
 
 #[derive(Clone)]
 #[allow(dead_code)]
--- validator/src/admin_rpc_service.rs
+++ validator/src/admin_rpc_service.rs
@@ -73,6 +73,7 @@ pub struct AdminRPCPostPackConfirmationConfig {
 
 #[derive(Clone)]
 pub struct AdminRpcRequestMetadata {
+    pub flags2: Arc<std::sync::atomic::AtomicU64>,
     pub rpc_addr: Option<SocketAddr>,
     pub start_time: SystemTime,
     pub start_progress: Arc<RwLock<ValidatorStartProgress>>,
@@ -399,6 +400,9 @@ pub trait AdminRpc {
         meta: Self::Metadata,
         blocklisted_uuids: Vec<String>,
     ) -> Result<()>;
+
+    #[rpc(meta, name = "enableExperimentalFeature")]
+    fn enable_experimental_feature(&self, meta: Self::Metadata, enable: bool) -> Result<()>;
 }
 
 pub struct AdminRpcImpl;
@@ -1236,6 +1240,15 @@ impl AdminRpc for AdminRpcImpl {
         })
     }
 
+    fn enable_experimental_feature(&self, meta: Self::Metadata, enable: bool) -> Result<()> {
+        if enable {
+            meta.flags2.fetch_or(1, Ordering::Relaxed);
+        } else {
+            meta.flags2.fetch_and(u64::MAX ^ 1, Ordering::Relaxed);
+        };
+        Ok(())
+    }
+
     fn is_generating_snapshots(&self, meta: Self::Metadata) -> Result<bool> {
         if let Some(snapshot_controller) = meta.snapshot_controller() {
             Ok(snapshot_controller.is_generating_snapshots())
@@ -1554,6 +1567,7 @@ mod tests {
                 Arc::new(ArcSwap::from_pointee(ShredReceiverAddresses::new()));
             let shred_retransmit_receiver_addresses = Arc::new(ArcSwap::default());
             let meta = AdminRpcRequestMetadata {
+                flags2: Arc::new(Default::default()),
                 rpc_addr: None,
                 start_time: SystemTime::now(),
                 start_progress,
@@ -1713,6 +1727,7 @@ mod tests {
 
             let post_init = Arc::new(RwLock::new(None));
             let meta = AdminRpcRequestMetadata {
+                flags2: Arc::new(Default::default()),
                 rpc_addr: validator_config.rpc_addrs.map(|(rpc_addr, _)| rpc_addr),
                 start_time: SystemTime::now(),
                 start_progress: start_progress.clone(),
@@ -1815,6 +1830,7 @@ mod tests {
 
         let post_init = Arc::new(RwLock::new(None));
         let meta = AdminRpcRequestMetadata {
+            flags2: Arc::default(),
             rpc_addr: None,
             start_time: SystemTime::now(),
             start_progress: start_progress.clone(),
@@ -1919,6 +1935,7 @@ mod tests {
         let start_progress = Arc::new(RwLock::new(ValidatorStartProgress::default()));
 
         let meta_no_post_init = AdminRpcRequestMetadata {
+            flags2: Arc::default(),
             rpc_addr: None,
             start_time: SystemTime::now(),
             start_progress,
